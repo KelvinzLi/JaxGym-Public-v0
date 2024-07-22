d3d109c5e487704610a2cdb52341158745913b95
NVIDIA <jax@nvidia.com>
2024-07-22 22:05:56 +0000

    PPO with KL update constraint

--- agents/PPO.py
+++ agents/PPO.py
@@ -8,10 +8,11 @@ from agents.policy_gradient_agent import PolicyGradient, ActorCriticDiscrete, Ac
 
 class PPO(PolicyGradient):
 
-    def __init__(self, advantage_estimator, clip_ratio, ppo_steps):
+    def __init__(self, advantage_estimator, clip_ratio, ppo_steps, target_kl = 0.01):
         super().__init__(advantage_estimator)
         self.clip_ratio = clip_ratio
         self.ppo_steps = ppo_steps
+        self.target_kl = target_kl
     
     @partial(jit, static_argnums=(0,))
     def train_one_step(self, actor, critic, obs, reward, action, done):
@@ -20,7 +21,7 @@ class PPO(PolicyGradient):
 
         old_advantage = jax.vmap(self.advantage_estimator, in_axes = (0, 0, 0))(old_pred_return, reward, done)
 
-        def ppo_update_step(ii, carry):
+        def ppo_update_step(carry):
             def loss_func(params):
                 actor_params, critic_params = params
 
@@ -37,25 +38,32 @@ class PPO(PolicyGradient):
 
                 actor_loss = surrogate_loss.mean()
 
-                return actor_loss + critic_loss, advantage[:, -1].sum()
+                approx_kl = (old_log_action_prob - log_action_prob).mean()
 
-            actor, critic, cumulative_loss, cumulative_aux = carry
+                return actor_loss + critic_loss, (approx_kl, actor_loss)
+
+            actor, critic, cumulative_loss, cumulative_aux, _, t = carry
 
             grad_fn = jax.value_and_grad(loss_func, has_aux = True)
-            (loss, aux), (actor_grads, critic_grads) = grad_fn((actor.params, critic.params))
+            (loss, (approx_kl, aux)), (actor_grads, critic_grads) = grad_fn((actor.params, critic.params))
 
             # aux = actor_grads['layers_0']['layers']['layers_0']['kernel'][0, 0]
 
             actor = actor.apply_gradients(grads=actor_grads)
             critic = critic.apply_gradients(grads=critic_grads)
 
-            carry = (actor, critic, cumulative_loss + loss, cumulative_aux + aux)
+            carry = (actor, critic, cumulative_loss + loss, cumulative_aux + aux, approx_kl, t + 1)
 
             return carry
 
-        carry = (actor, critic, 0, 0)
-        carry = jax.lax.fori_loop(0, self.ppo_steps, ppo_update_step, carry)
-        actor, critic, loss, aux = carry
+        print("Using while loop")
+        
+        carry = (actor, critic, 0, 0, 0, 0)
+        carry = jax.lax.while_loop(lambda carry: jnp.greater(self.ppo_steps, carry[-1]) & jnp.greater(1.5 * self.target_kl, carry[-2]), 
+                                   ppo_update_step, 
+                                   carry,
+                                  )
+        actor, critic, loss, aux, _, _ = carry
 
         return actor, critic, loss, aux
 
